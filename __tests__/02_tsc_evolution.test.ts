09f6cf5f6a6e43445161cda8f020af181acc8ee3
Robert Całka <calka.rob@gmail.com>
2018-08-08 22:51:28 +0200

    update ts-jest and tests

--- __tests__/02_tsc_evolution.test.ts
+++ __tests__/02_tsc_evolution.test.ts
@@ -1,10 +1,11 @@
 describe("typescript - evolution", () => {
     test("01 - null and undefined = non nullable types", () => {
-        // let name
+        let name:string;
 
-        // name = "Rob";
-        // name = null;
-        // name = undefined;
+        name = "Rob";
+        name = null;
+        name = undefined;
+        
         expect(2).toBe(2);
     })    
 })
